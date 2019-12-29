bc4ed69208c9e967343e156ee63027f4371883be
GMaiolo <guille.maiolo@gmail.com>
2019-12-29 20:10:52 -0300

    playing with cloudinary

--- src/components/FeedImagePlaceholder.vue
+++ src/components/FeedImagePlaceholder.vue
@@ -4,8 +4,8 @@
     height="300"
     width="300"
     min-width="300"
-    :src="src"
-    :lazy-src="lazySrc"
+    :src="`https://picsum.photos/300/300?image=${randomNumber}`"
+    :lazy-src="`https://picsum.photos/10/10?image=${randomNumber}`"
   >
     <template v-slot:placeholder>
       <v-row class="fill-height ma-0" align="center" justify="center">
@@ -16,24 +16,10 @@
 </template>
 
 <script>
-const baseUrl = "https://res.cloudinary.com/gmaiolo/image/upload";
-
 export default {
-  name: "FeedImage",
-  props: {
-    path: {
-      type: String,
-      required: true
-    }
-  },
-  computed: {
-    src() {
-      return `${baseUrl}/w_300,h_300/${this.path}`;
-    },
-    lazySrc() {
-      return `${baseUrl}/t_media_lib_thumb/${this.path}`;
-    }
-  }
+  data: () => ({
+    randomNumber: Math.floor(Math.random() * 100 + 1)
+  })
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,15 @@
   <v-container>
     <v-row>
       <v-col class="py-8 text-center">
-        <div v-for="n in 25" :key="n" class="d-inline-block mx-1">
-          <FeedImage />
+        <div
+          v-for="imagePath in imagePaths"
+          :key="imagePath"
+          class="d-inline-block mx-1"
+        >
+          <FeedImage :path="imagePath" />
+        </div>
+        <div v-for="n in 23" :key="n" class="d-inline-block mx-1">
+          <FeedImagePlaceholder />
         </div>
       </v-col>
     </v-row>
@@ -11,12 +18,17 @@
 </template>
 
 <script>
+import FeedImagePlaceholder from "../components/FeedImagePlaceholder";
 import FeedImage from "../components/FeedImage";
 
 export default {
   name: "home",
   components: {
+    FeedImagePlaceholder,
     FeedImage
-  }
+  },
+  data: () => ({
+    imagePaths: ["/s33d/fur.jpg", "/s33d/flower.png"]
+  })
 };
 </script>
