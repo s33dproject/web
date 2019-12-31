88d6f7de759c16de843c43f993f806b1a54f921e
julietaflux <julietaflux@gmail.com>
2019-12-31 14:00:49 -0300

    + artoworks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,7 @@
   <v-container class="py-8">
     <v-row>
       <v-col class="text-center">
-        <figure
-          v-for="imageName in imageNames"
-          :key="imageName"
-          class="d-inline-block mx-1"
-        >
+        <figure v-for="imageName in imageNames" :key="imageName" class="d-inline-block mx-1">
           <FeedImage :name="imageName" />
         </figure>
       </v-col>
@@ -24,15 +20,65 @@ export default {
   },
   data: () => ({
     imageNames: [
-      "bw_fur",
+      "fur_indigo_tp",
+      "fur_green_tp",
+      "fur_yellow_tp",
+      "fur_violet_tp",
+      "fur_red_tp",
+      "fur_orange_tp",
+      "fur_black_tp",
+      "noname_I",
+      "noname_II",
+      "noname_III",
+      "noname_IV",
+      "noname_V",
+      "noname_VI",
+      "noname_VII",
+      "noname_VIII",
+      "noname_IX",
+      "noname_X",
+      "noname_XI",
+      "noname_XII",
+      "geometry_platforms_I",
+      "geometry_platforms_II",
+      "geometry_platforms_III",
+      "geometry_platforms_IV",
+      "geometry_platforms_V",
+      "geometry_platforms_VI",
+      "geometry_platforms_VII",
+      "geometry_platforms_VIII",
+      "hodgepodge_colors_I",
+      "hodgepodge_colors_III",
+      "hodgepodge_colors_V",
+      "hodgepodge_colors_VI",
+      "hodgepodge_colors_VIII",
+      "hodgepodge_colors_XI",
+      "hodgepodge_colors_XIV",
+      "hodgepodge_colors_XV",
+      "hodgepodge_colors_XVI",
+      "dither_I",
       "flower",
-      "chars",
-      "pacmen_1",
-      "chars_1",
-      "pacmen_2",
-      "chars_0",
-      "flower_5",
-      "pacmen_5"
+      "origin_I",
+      "origin_XI",
+      "origin_V",
+      "pixel_shifting_I",
+      "pixel_shifting_II",
+      "pixel_shifting_III",
+      "pixel_shifting_IV",
+      "carnation_flower_I",
+      "carnation_flower_IV",
+      "carnation_flower_V",
+      "carnation_flower_XIV",
+      "carnation_flower_XV",
+      "hodgepodge_I",
+      "hodgepodge_IV",
+      "hodgepodge_V",
+      "hodgepodge_VII",
+      "hodgepodge_X",
+      "hodgepodge_XI",
+      "layers_I",
+      "layers_II",
+      "layers_III"
     ]
   })
 };
